541d047b4421bf6ffd523d7296218c02538c7a35
Henry Nguyen <henry@henrythehippie.com>
2021-04-11 23:28:28 -0700

    Add disabled states

--- looptube.js
+++ looptube.js
@@ -1,26 +1,39 @@
-document.getElementById('markStart').addEventListener('click', function() { markStart() });
-document.getElementById('decrementStart').addEventListener('click', function() { decrementStart() });
-document.getElementById('incrementStart').addEventListener('click', function() { incrementStart() });
-document.getElementById('markEnd').addEventListener('click', function() { markEnd() });
-document.getElementById('decrementEnd').addEventListener('click', function() { decrementEnd() });
-document.getElementById('incrementEnd').addEventListener('click', function() { incrementEnd() });
-document.getElementById('startLoop').addEventListener('click', function() { startLoop() });
-document.getElementById('stopLoop').addEventListener('click', function() { stopLoop() });
+let markStartButton = document.getElementById('markStart');
+let decrementStartButton = document.getElementById('decrementStart');
+let incrementStartButton = document.getElementById('incrementStart');
 
-var progressBar = document.getElementById('progressBar');
-var startTimeLabel = document.getElementById('startTimeLabel');
-var endTimeLabel = document.getElementById('endTimeLabel');
+let markEndButton = document.getElementById('markEnd');
+let decrementEndButton = document.getElementById('decrementEnd');
+let incrementEndButton = document.getElementById('incrementEnd');
+
+let startLoopButton = document.getElementById('startLoop');
+let stopLoopButton = document.getElementById('stopLoop');
+
+let progressBar = document.getElementById('progressBar');
+let startTimeLabel = document.getElementById('startTimeLabel');
+let endTimeLabel = document.getElementById('endTimeLabel');
+
+markStartButton.addEventListener('click', function() { markStart() });
+decrementStartButton.addEventListener('click', function() { decrementStart() });
+incrementStartButton.addEventListener('click', function() { incrementStart() });
+
+markEndButton.addEventListener('click', function() { markEnd() });
+decrementEndButton.addEventListener('click', function() { decrementEnd() });
+incrementEndButton.addEventListener('click', function() { incrementEnd() });
+
+startLoopButton.addEventListener('click', function() { startLoop() });
+stopLoopButton.addEventListener('click', function() { stopLoop() });
 
 const MILLISECONDS_PER_SECOND = 1000;
 const MIN_LOOP_DURATION_IN_SECONDS = 1;
 const INCREMENT = .10;
 
-var progressIntervalId;
-var loopIntervalId;
-var loopStart = 0;
-var loopEnd = 0;
-var loopDurationInSeconds = 0;
-var player;
+let progressIntervalId;
+let loopIntervalId;
+let loopStart = 0;
+let loopEnd = 0;
+let loopDurationInSeconds = 0;
+let player;
 
 function onYouTubeIframeAPIReady() {
   let videoId = new URLSearchParams(window.location.search).get('v');
@@ -34,6 +47,9 @@ function onYouTubeIframeAPIReady() {
 function markStart() {
   loopStart = player.getCurrentTime();
   startTimeLabel.value = getDisplayTime(loopStart);
+  markEndButton.disabled = false;
+  decrementStartButton.disabled = false;
+  incrementStartButton.disabled = false;
   startLoop();
 }
 
@@ -52,6 +68,8 @@ function incrementStart() {
 function markEnd() {
   loopEnd = player.getCurrentTime();
   endTimeLabel.value = getDisplayTime(loopEnd);
+  decrementEndButton.disabled = false;
+  incrementEndButton.disabled = false;
   startLoop();
 }
 
@@ -70,18 +88,22 @@ function incrementEnd() {
 function startLoop() {
   loopDurationInSeconds = loopEnd - loopStart;
   if (loopDurationInSeconds < MIN_LOOP_DURATION_IN_SECONDS) return;
+  stopLoopButton.disabled = false;
   stopLoop();
   restartLoop();
   loopIntervalId = setInterval(restartLoop, loopDurationInSeconds * MILLISECONDS_PER_SECOND);
 }
 
 function stopLoop() {
+  startLoopButton.disabled = false;
+  stopLoopButton.disabled = true;
   clearInterval(loopIntervalId);
   clearInterval(progressIntervalId);
   progressBar.style.width = "0%";
 }
 
 function restartLoop() {
+  stopLoopButton.disabled = false;
   player.seekTo(loopStart, true);
   clearInterval(progressIntervalId);
   progressBar.style.width = "0%";
